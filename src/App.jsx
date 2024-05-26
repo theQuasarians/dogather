51024b5cd7b4f5acf0665b55db4b88b837630500
Boualam3 <boualam.bourahla@gmail.com>
2024-05-26 20:50:02 +0100

    add illustartion in mobile view ;

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./sections/Hero"
 function App() {
   return (
     <>
+    {/* this should be Navbar here instead of div*/}
       <div style={{ minHeight: "10vh", border: "2px solid black" }}>
         Nav bar
       </div>
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,16 +1,21 @@
 import classes from "../styles/Hero.module.css"
-
+import potato_man_img from "../assets/potato_man.svg"
 const Hero = () => {
   return (
     <div className={classes.hero__container}>
       <div className={classes.hero__center_box}>
-        <h1 className={classes.hero__title}>
-          Let your idea find the right people
-        </h1>
-        <p className={classes.hero__description}>
-          Connect with people and build your dream projects together
-        </p>
-        <button className={classes.hero__btn}>get started</button>
+        <div className={classes.hero__box_content}>
+          <h1 className={classes.hero__title}>
+            Let your idea find the right people
+          </h1>
+          <p className={classes.hero__description}>
+            Connect with people and build your dream projects together
+          </p>
+          <button className={classes.hero__btn}>get started</button>
+        </div>
+        <div className={classes.hero__box_illustration}>
+          <img className={classes.hero__potato_man} src={potato_man_img} />
+        </div>
       </div>
     </div>
   )
