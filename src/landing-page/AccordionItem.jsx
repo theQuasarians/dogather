b5511cd1a623e04ef8765db10c21a2da3e3238ca
Cheljee Mangulabnan <cheljee.mangulabnan@gmail.com>
2024-05-26 22:32:12 +0800

    desktop faq section

---
+++ src/landing-page/AccordionItem.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+
+const AccordionItem = ({ data }) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggleOpen = () => {
+    setIsOpen(!isOpen)
+  }
+  return (
+    <div className='accordion-item'>
+      <div className='accordion-item__title-group'>
+        <h4 className='accordion-item__title'>{data.question}</h4>
+        {isOpen ?
+          <button onClick={toggleOpen} className='accordion-item__close'>
+            <img src='../../public/close-icon.svg' />
+          </button>
+          :
+          <button onClick={toggleOpen} className='accordion-item__open'>
+            <img src='../../public/open-icon.svg' />
+          </button>
+        }
+      </div>
+      <p className={`accordion-item__content ${isOpen ? '' : 'hidden'}`}>{data.answer}</p>
+    </div>
+  )
+}
+
+export default AccordionItem
---
+++ src/landing-page/FaqSection.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import './faq-section.css'
+
+import AccordionItem from './AccordionItem'
+
+const FaqSection = () => {
+  const faqData = [
+    {
+      id: 1,
+      question: "How does the app connect me to the people I can build projects with",
+      answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam mollis leo hendrerit magna ullamcorper, quis ultricies magna fermentum."
+    },
+
+    {
+      id: 2,
+      question: "What skills should I have to connect with people",
+      answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam mollis leo hendrerit magna ullamcorper, quis ultricies magna fermentum."
+    },
+
+    {
+      id: 3,
+      question: "How does this app help me to gather a team and launch my idea",
+      answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam mollis leo hendrerit magna ullamcorper, quis ultricies magna fermentum."
+    },
+    {
+      id: 4,
+      question: "Should I be professional to gather people as my team",
+      answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam mollis leo hendrerit magna ullamcorper, quis ultricies magna fermentum."
+    },
+    {
+      id: 5,
+      question: "Should I be professional to gather people as my team",
+      answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam mollis leo hendrerit magna ullamcorper, quis ultricies magna fermentum."
+    }
+  ]
+  return (
+    <section className='faq-section'>
+      <h3 className='faq-section__heading'>Questions & answers</h3>
+      <div className='faq-section__accordion-wrapper'>
+        {faqData.map(data => (
+          <AccordionItem key={data.id} data={data}></AccordionItem>
+        ))}
+
+
+      </div>
+
+    </section >
+  )
+}
+
+export default FaqSection
