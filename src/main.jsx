3a7e9e4b13fd2c7f44cb1e531469738cda6fd015
Agustin <76200509+AgusZ02@users.noreply.github.com>
2024-05-28 16:02:56 +0200

    chore: Remove my component from main
    Removed my component from main.jsx to fulfill rule 5 of code review "Do not insert your component into the main react Render".

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import CTA from './components/CTA/CTA.jsx'
+import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CTA />
+    <App />
   </React.StrictMode>,
 )
