c1693c70637a0ef5880c51c9007c9419ff7e2b85
Agustin <76200509+AgusZ02@users.noreply.github.com>
2024-05-28 23:21:33 +0200

    review: Applied changes from review
    Review with @skyc4ff3ine
    - removed root style from CTA style
    - removed display flex from body -> added new container to center elements.
    - removed redundant styles like border-box, flex-direction:row and padding: 0 margin: 0

--- src/components/CTA/CTA.jsx
+++ src/components/CTA/CTA.jsx
@@ -4,16 +4,14 @@ import './CTA.css'
 
 function CTA() {
   return (
-    <>
-      <div id='container'>
+    <div id='center-container'>
+      <div id='cta-container'>
         <img id='cta-image' src={image} alt="CTA Illustration" />
         <h3>Get started with building your ideas</h3>
-        <div id='email-form'>
-          <input id='input-email' type='email' placeholder='Enter your email...' />
-          <button id='sign-up-button'>sign up</button>
-        </div>
+        <input id='input-email' type='email' placeholder='Enter your email...' />
+        <button id='sign-up-button'>sign up</button>
       </div>
-    </>
+    </div>
   )
 }
 
