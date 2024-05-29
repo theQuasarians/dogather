943bff5b734e2fb60be05fc0ca3ec4e1802c6d49
karan pun <karanpun011@gmail.com>
2024-05-29 15:15:15 -0400

    add responsive design

---
+++ src/app/App.jsx
@@ -0,0 +1,13 @@
+
+import Hero from '../components/Hero.jsx'
+
+function App() {
+
+  return (
+    <>
+      <Hero/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './hero.css'
+const Hero = () => {
+    return (
+        <div>
+            <div className='nav'> dogather</div>
+            <div className='container'>
+            <div className='main-text'> Let your idea find the right people </div>
+            <div className='sub-text'> Connect with people and build your dream projects together</div>          
+            <div><button className='button-get-started'>get started</button></div>
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
