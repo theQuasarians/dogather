21bb24abcdf5042ff3573f9af5a0ccadb3d377b4
Agustin <76200509+AgusZ02@users.noreply.github.com>
2024-05-31 13:08:35 +0200

    Replaced png illustration with svg
    Svg in public/

--- src/components/CTA/CTA.jsx
+++ src/components/CTA/CTA.jsx
@@ -1,12 +1,12 @@
 
-import image from '../../../public/CTA Illustration.png'
+import ctaSvg from '/ctaIllustration.svg'
 import './CTA.css'
 
 function CTA() {
   return (
     <div id='center-container'>
       <div id='cta-container'>
-        <img id='cta-image' src={image} alt="CTA Illustration" />
+        <img id='cta-image' src={ctaSvg} alt="CTA Illustration" />
         <h3>Get started with building your ideas</h3>
         <input id='input-email' type='email' placeholder='Enter your email...' />
         <button id='sign-up-button'>sign up</button>
