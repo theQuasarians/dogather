e5fe2989beb8b28b92978faf721616796f1a2cf7
zaynabzaryoun <anaab@gmail.com>
2024-06-18 21:43:25 +0330

    hero section

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+import './Hero.css';
+
+const Hero = () => {
+  return (
+    <section className="container">
+      <h2 className="title">Let your idea find the right people</h2>
+      <p className="subtitle">Connect with people and build your dream projects together</p>
+      <button className="button">get started</button>
+    </section>
+  )
+};
+
+export default Hero;
