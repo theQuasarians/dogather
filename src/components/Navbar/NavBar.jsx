5c6a4d65d72d5fcc99b102c551b8bb7dc8784604
mohamed <mohamedabbj@gmail.com>
2024-06-25 18:50:25 -0400

    Fixing register button to link and separating NavbarLinks outside of main component, fixing style and adding more comments

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -18,18 +18,15 @@ function NavBar() {
       name: "contact",
       id: 3,
     },
+    {
+      name: "login",
+      id: 4,
+    },
+    {
+      name: "register",
+      id: 5,
+    },
   ];
-
-  /* Variables that contain login and register links */
-  const loginLink = {
-    name: "login",
-    id: 4,
-  };
-
-  const registerLink = {
-    name: "register",
-    id: 5,
-  };
 
   /* Remove content if browser width goes beyond mobile */
   useEffect(() => {
@@ -39,62 +36,6 @@ function NavBar() {
       } 
     })
   }, [])
-  
-
-  /* Component that returns the links */
-  function PageButtons(element) {
-    
-    return (
-      <>
-        <a
-          key={element.id}
-          id={`${element.name.replace(" ", "-")}-link`}
-          href={`/${element.name.replace(" ", "")}`}
-        >
-          {element.name}
-          <img src="../src/assets/navbarAssets/arrowLink.svg" alt="arrowLink" />
-          <img src={`../src/assets/navbarAssets/${element.name.replace(' ','')}.svg`} alt="mobileIcon" />
-        </a>
-      </>
-    );
-  }
-
-  /* Component that returns the Register and Login links */
-  function RegisterLoginContent(content) {
-    return(
-      <>
-      {/* This will return the version that was passed as props to the component */}
-         <div className="register-login">
-          <a
-            key={loginLink.id}
-            id={`${loginLink.name}-link`}
-            href={`/${loginLink.name.replace(" ", "")}`}
-          >
-            {loginLink.name}
-            <img src="../src/assets/navbarAssets/arrowLink.svg" alt="ArrowLink" />
-            <img src={`../src/assets/navbarAssets/${loginLink.name}.svg`} alt="mobileIcon" />
-          </a>
-        {content.type == 'mobile' ?
-           <a
-           key={registerLink.id}
-           id={`${registerLink.name}-link`}
-           href={`/${registerLink.name.replace(" ", "")}`}
-         >
-           {registerLink.name}
-         <img  src="../src/assets/navbarAssets/ArrowLink.svg" alt="arrowLink" />
-         <img  src={`../src/assets/navbarAssets/${registerLink.name}.svg`} alt="mobileIcon" />
-         </a>
-          :
-          /* make this a link */
-          <div className="register-btn">
-            <button>register</button>
-          </div>
-      
-      }
-        </div>
-      </>
-    )
-  }
 
 
   return (
@@ -111,12 +52,9 @@ function NavBar() {
         {/* Buttons get created thanks to array prototype.map */}
         <div className="pages-links">
           {pageLinks.map((element) => (
-            <PageButtons name={element.name} key={element.id} />
+            <NavbarLinks name={element.name} key={element.id} />
           ))}
         </div>
-        {/* Component gets called passing type='desktop' as props, so it will show the 
-        respective links in the desktop version */}
-        <RegisterLoginContent type='desktop'/>
         {/* Content of the mobile menu */}
         <div className="mobile-menu">
           <button onClick={() => setToggleMobileMenu(!toggleMobileMenu)}>
@@ -135,15 +73,30 @@ function NavBar() {
         <div style={{display: toggleMobileMenu ? 'block' : 'none'}} className="mobile-menu-wrapper">
           <div className="pages-links">
            {pageLinks.map((element) => (
-            <PageButtons name={element.name} key={element.id} />
+            <NavbarLinks name={element.name} key={element.id} />
           ))}
           </div>
-             {/* Component gets called passing type='desktop' as props, so it will show the 
-               respective links in the mobile version */}
-          <RegisterLoginContent type='mobile'/>
         </div>
     </>
   );
 }
 
 export default NavBar;
+
+
+//Component that renders the NavbarLinks
+function NavbarLinks(element) {
+  return (
+    <>
+      <a
+        key={element.id}
+        id={`${element.name.replace(" ", "-")}-link`}
+        href={`/${element.name.replace(" ", "")}`}
+      >
+        {element.name}
+        <img src="../src/assets/navbarAssets/arrowLink.svg" alt="arrowLink" />
+        <img src={`../src/assets/navbarAssets/${element.name.replace(' ','')}.svg`} alt="mobileIcon" />
+      </a>
+    </>
+  );
+}
