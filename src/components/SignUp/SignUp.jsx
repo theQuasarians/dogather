3876faf9bd245b2de6b2ab105cb698e5bf6e698d
Boualam3 <boualam.bourahla@gmail.com>
2024-07-02 20:17:55 +0100

    made singup validation

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,8 +1,49 @@
-import "./SignUp.css";
+import "./SignUp.css"
 
-import logoSignUp from "../../assets/signupAssets/logo.svg";
-import eyes from "../../assets/signupAssets/eyes.svg";
+import logoSignUp from "../../assets/signupAssets/logo.svg"
+import eyes from "../../assets/signupAssets/eyes.svg"
+import { useEffect, useState } from "react"
+import { validateSignupForm } from "../../utils/validateData.js"
 export default function SignUp() {
+  const [showPassword, setShowPassword] = useState(false)
+  const [signupForm, setSignupForm] = useState({
+    username: "",
+    email: "",
+    password: "",
+    errors: {
+      username: "",
+      email: "",
+      password: "",
+    },
+  })
+
+  const fillDataFields = (e) => {
+    // console.log(object);
+    if (e.target?.name) {
+      const { name } = e.target
+      setSignupForm((prev) => ({ ...prev, [name]: e.target.value }))
+    }
+  }
+
+  const submitForm = (e) => {
+    e.preventDefault()
+    const errors = validateSignupForm(signupForm)
+    // console.log("vaildtion fun", errors)
+    if (
+      errors.username?.length |
+      errors.password?.length |
+      errors.email?.length
+    ) {
+      setSignupForm((prev) => ({ ...prev, errors }))
+    } else {
+      // post data to the server
+      // console.log(signupForm)
+    }
+  }
+  useEffect(() => {
+    console.log(signupForm)
+  }, [signupForm])
+
   return (
     <>
       <nav className="signup__nav">
@@ -16,31 +57,50 @@ export default function SignUp() {
             <h2> Sign up </h2>
             <label htmlFor="username">Your username</label>
             <input
+              onChange={(e) => fillDataFields(e)}
               type="text"
               name="username"
               id="username"
               placeholder="Enter your username"
               required
+              className={`${signupForm.errors.username && "username_error"}`}
             />
+            {signupForm.errors.username && (
+              <small>{signupForm.errors.username}</small>
+            )}
             <label htmlFor="email">Your email</label>
             <input
+              onChange={(e) => fillDataFields(e)}
               type="email"
               name="email"
               id="email"
               placeholder="Enter your email address"
+              className={`${signupForm.errors.email && "email_error"}`}
               required
             />
+            {signupForm.errors.email && (
+              <small>{signupForm.errors.email}</small>
+            )}
             <label htmlFor="password">Your password</label>
             <div className="passwordContainer">
               <input
-                type="password"
+                onChange={(e) => fillDataFields(e)}
+                type={`${showPassword ? "text" : "password"}`}
                 name="password"
                 id="password"
                 placeholder="Enter your password"
                 required
+                className={`${signupForm.errors.password && "password_error"}`}
               />
-
-              <img src={eyes} alt="eyes-password" className="password__eyes" />
+              {signupForm.errors.password && (
+                <small>{signupForm.errors.password}</small>
+              )}
+              <img
+                src={showPassword ? eyes : eyes}
+                onClick={() => setShowPassword(!showPassword)}
+                alt="eyes-password"
+                className="password__eyes"
+              />
             </div>
             <p className="form__linkLabel">
               Already have an account?
@@ -51,12 +111,16 @@ export default function SignUp() {
                 </a>
               </span>
             </p>
-            <button type="submit" className="form__cta">
+            <button
+              onClick={(e) => submitForm(e)}
+              type="submit"
+              className="form__cta"
+            >
               Sign up
             </button>
           </form>
         </div>
       </div>
     </>
-  );
+  )
 }
